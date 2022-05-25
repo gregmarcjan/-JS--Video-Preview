fe44c7a78ae0284e6d4b5e6dc45821a281e29367
Greg Marcjan <gmarcjan@gmail.com>
2022-05-24 21:41:10 -0700

    complete

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+function playVideo(element) {
+    element.play();
+    element.muted = true;
+}
+
+
+function pauseVideo(element) {
+    element.pause();
+    element.muted = false;
+}
+
